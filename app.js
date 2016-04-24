25e57efb5fdabb153372981e5bcf2067d49a30f8
Rockncoder <rockncoder@gmail.com>
2016-04-24 15:11:24 -0700

    first api

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var api = require('./routes/api');
 
 var app = express();
 
@@ -24,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-
+app.use('/api', api);
 
 app.all('/*', function(req, res) {
   console.log(JSON.stringify(req.Url));
